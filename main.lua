30da383bcc8459b4f8bf4a48e86c13ff9488bde2
Pixeluted <78791776+Pixeluted@users.noreply.github.com>
2022-02-04 12:02:16 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,52 @@
+local Player = game.Players.LocalPlayer
+local PcItems = Player:WaitForChild("PcItems")
+
+local Remotes = game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents
+local Shop = workspace.Shop
+
+
+local function placeCase(caseType)
+    Remotes.PlaceItem:FireServer(caseType)
+    Remotes.BuildPc:FireServer("finish editing component")
+end
+
+local function placeMotherboard(MotherboardObject)
+    Remotes.PlacePart:FireServer("Motherboard", MotherboardObject)
+    Remotes.BuildPc:FireServer("groupPart")
+end
+
+local function placePowerSuply(PowerSupplyObject)
+    Remotes.PlacePart:FireServer("PowerSuply", PowerSupplyObject)
+    Remotes.BuildPc:FireServer("groupPart")
+end
+
+local function placeCPU(CPUObject)
+    Remotes.PlacePart:FireServer("Cpu", CPUObject)
+    Remotes.BuildPc:FireServer("groupPart") 
+end
+
+local function placeGPU(GPUObject)
+    Remotes.PlacePart:FireServer("Gpu", GPUObject)
+    Remotes.BuildPc:FireServer("groupPart") 
+end
+
+
+local function placeRAM(RAMObject)
+    Remotes.PlacePart:FireServer("Ram", RAMObject)
+    Remotes.BuildPc:FireServer("groupPart") 
+end
+
+local function placeMemory(MemoryObject)
+    Remotes.PlacePart:FireServer("Memory", MemoryObject)
+    Remotes.BuildPc:FireServer("groupPart")   
+end
+
+local function placeFan(FanObject)
+    Remotes.PlacePart:FireServer("Fan", FanObject)
+    Remotes.BuildPc:FireServer("groupPart") 
+end
+
+local function placeOtherCooling(OtherObject)
+    Remotes.PlacePart:FireServer("Air", OtherObject)
+    Remotes.BuildPc:FireServer("groupPart") 
+end
