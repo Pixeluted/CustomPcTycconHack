463e4ecb104d1c36481372e0ebaaeabf31fecb5e
Pixeluted <78791776+Pixeluted@users.noreply.github.com>
2022-02-04 12:19:08 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,88 @@
-print("Main scipt loaded")
-
 local Player = game.Players.LocalPlayer
 local PcItems = Player:WaitForChild("PcItems")
 
 local Remotes = game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents
 local Shop = workspace.Shop
 
-local shopItemsModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/Pixeluted/CustomPcTycconHack/main/shopItems.lua))()
-print(shopItemsModule)
-shopItemsModule.indexAllItems()
+local shopItemsInfo = {}
 
+--// Indexing all items
+
+function getPriceAndCurrency(priceValue)
+    local price = string.gsub(priceValue, "%D", "")
+    local currency
+    
+    if string.find(priceValue, "Cash") then
+        currency = "Cash"
+    elseif string.find(priceValue, "Gold") then
+        currency = "Gold"
+    end
+    
+    return price, currency
+end
+
+function getNeededValue(theParent)
+      for _,v in pairs(theParent:GetChildren()) do
+            if v.Name == "BuyButton" then
+                for _,ok in pairs(v:GetChildren()) do
+                    if ok.Name == "Attachment" then
+                        for _,oks in pairs(ok:GetChildren()) do
+                            if oks.Name == "ProximityPrompt" then
+                                for _,okse in pairs(oks:GetChildren()) do 
+                                    if okse.Name == "Cost" then
+                                        return okse.Value  
+                                    end
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+end
+
+function indexCategory(theFolder)
+    local gatheredInfo = {}
+  
+    for _,theItem in pairs(theFolder:GetChildren()) do
+        local valueToConvert = getNeededValue(theItem)
+        
+        local price, theCurrency
+        
+        task.spawn(function()
+            price, theCurrency = getPriceAndCurrency(valueToConvert)
+        end)
+        
+        print()
+        print(theItem.Name)
+        print("PRICE: ", price)
+        print("CURRENCY: ", theCurrency)
+    
+        gatheredInfo[theItem.Name] = {
+          ["Price"] = price,
+          ["Currency"] = theCurrency
+        }
+    end
+  
+    return gatheredInfo
+end
+
+
+function getAllTypes()
+    for _,category in pairs(Shop:GetChildren()) do
+        if category:IsA("Folder") then
+            if category.Name ~= "Buttons" then
+                print()
+                print(category.Name)
+                shopItemsInfo[category.Name] = indexCategory(category)
+            end
+        end
+    end
+end
+
+getAllTypes()
+
+--// PC Building Functions
 local function placeCase(caseType)
     Remotes.PlaceItem:FireServer(caseType)
     Remotes.BuildPc:FireServer("finish editing component")
@@ -55,3 +128,5 @@ local function placeOtherCooling(OtherObject)
     Remotes.PlacePart:FireServer("Air", OtherObject)
     Remotes.BuildPc:FireServer("groupPart") 
 end
+
+print("Main scipt loaded")
