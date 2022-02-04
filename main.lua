c36add12cfddeb7b338ab087dff18529e6818157
Pixeluted <78791776+Pixeluted@users.noreply.github.com>
2022-02-04 12:13:55 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ local PcItems = Player:WaitForChild("PcItems")
 local Remotes = game:GetService("ReplicatedStorage").Resources.Remotes.RemoteEvents
 local Shop = workspace.Shop
 
-local shopItemsModule = loadstring(game.HtppService:GetAsync("https://github.com/Pixeluted/CustomPcTycconHack/raw/main/shopItems.lua", true))()
+local shopItemsModule = loadstring(game:HttpGet("https://github.com/Pixeluted/CustomPcTycconHack/raw/main/shopItems.lua"))()
 shopItemsModule.indexAllItems()
 
 local function placeCase(caseType)
